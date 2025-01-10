15144723f7f5090652fde7d9caf2c91374e77d8e
xaatu <xaatu>
2025-01-10 15:31:35 +0000

    updated readme to include bash command used to create file

--- obj/Debug/net9.0/AllTheBeansApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/AllTheBeansApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AllTheBeansApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26f568252e040ac69ff34d7d9ec15903b67229e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c574695a4f63673710d5a399ced75ff425898e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AllTheBeansApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AllTheBeansApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
